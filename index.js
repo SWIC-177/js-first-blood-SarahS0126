cb55763761fd0242b732221944b4af9577432f98
SarahS0126 <156946473+SarahS0126@users.noreply.github.com>
2024-04-06 23:41:29 -0500

    add example problem

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-const addCats = (cat1, cat2) => cat1 +cat2;
-
-const cat = {
-name: "Pickles",
-age: 2,
-isAdult: true
-};
-
-function createGreeting(individual) {
-  return (salutation) => `${salutation}, ${individual.name}!!`;
+function createScoreIncrementer(score) {
+  return function addScore(currentScore) {
+    return currentScore + score;
+  }
 }
-
-const greetPickles = createGreeting(cat)
-console.log(greetPickles("Howdy"));
-console.log(greetPickles("Good Morning"));
-
-const greetFluffy = createGreeting({
-  name: "Fluffy",
-  age: 4,
-  isAdult: true
-  });
-console.log(greetFluffy("Hello"));
-console.log(greetFluffy("Good Afternoon"));
-
-// this is more!
-
-function createMultiplier(factor) {
-  return (number) => number * factor;
-}
-const double = createMultiplier(2);
-console.log(double(5));
-console.log(double(10));
+let testScore = createScoreIncrementer(1)(2)
+console.log (`Score:${testScore}`)
