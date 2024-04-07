0b43f47846c44f4b071bb3eb9b2f29d1beaf55b2
SarahS0126 <156946473+SarahS0126@users.noreply.github.com>
2024-04-06 21:42:18 -0500

    emulate first video

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function greet(name)
-{
-    return "Hello " + name + "!";
-}  
+function addCats(cat1,cat2) {
+  return cat1 + cat2;
+}
+
+const addCats = (cat1, cat2) => {
+  console.log(cat1 + cat2);
+
+  return cat1 + cat2
+}
