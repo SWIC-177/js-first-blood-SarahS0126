76647cd14d300842fe6c07c2bf1e6eca8f6d6ad1
SarahS0126 <156946473+SarahS0126@users.noreply.github.com>
2024-04-06 22:08:26 -0500

    emulate video 3

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-function welcome(name) {
-  return `Welcome, ${name}!!`;
+const addCats = (cat1, cat2) => cat1 +cat2;
+
+const cat = {
+name: "Pickles",
+age: 2,
+isAdult: true
+};
+
+function createGreeting(individual) {
+  return (salutation) => `${salutation}, ${individual.name}!!`;
 }
-function processPerson(name, func) {
-  return func(name);
+
+const greetPickles = createGreeting(cat)
+console.log(greetPickles("Howdy"));
+console.log(greetPickles("Good Morning"));
+
+const greetFluffy = createGreeting({
+  name: "Fluffy",
+  age: 4,
+  isAdult: true
+  });
+console.log(greetFluffy("Hello"));
+console.log(greetFluffy("Good Afternoon"));
+
+// this is more!
+
+function createMultiplier(factor) {
+  return (number) => number * factor;
 }
-console.log(processPerson(`Sarah`,welcome));
+const double = createMultiplier(2);
+console.log(double(5));
+console.log(double(10));
