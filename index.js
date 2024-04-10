60003a350eee2c934be4b562796ecb1e0b150047
SarahS0126 <156946473+SarahS0126@users.noreply.github.com>
2024-04-09 20:26:17 -0500

    add pass by value example

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function createScoreIncrementer(score) {
-  return function addScore(currentScore) {
-    return currentScore + score;
-  }
+const younger1 = 13;
+let younger2 = younger1;
+
+function incrementAge(age) {
+    return age + 1;
 }
-let testScore = createScoreIncrementer(1)(2)
-console.log (`Score:${testScore}`)
+
+younger2 = incrementAge(younger2);
+
+console.log(`Younger1 is: ${younger1}`);
+console.log(`Younger2 is: ${younger2}`);
